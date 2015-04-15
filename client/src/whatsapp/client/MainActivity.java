5413b3544d904b818354b1b74678b0bc508b9250
MatSebMan <matiasmanzano@yahoo.com.ar>
2015-04-15 16:43:49 -0300

    Agregado de Vista principal y vista de Mensajes (kickoff)

---
+++ client/src/whatsapp/client/MainActivity.java
@@ -0,0 +1,77 @@
+package whatsapp.client;
+
+import model.LoginService;
+import model.ServerResultReceiver;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity implements ServerResultReceiver.Listener{
+    private static final String REGISTERED = "registered";
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        if (!isUserRegistered())
+            setContentView(R.layout.login);
+        else 
+            Log.v("REGISTRO", "tengo que a la proxima activity porque ya estoy registrado");
+        //TODO: ir a la proxima activity
+
+    }
+
+    public void register(View v){
+        Bundle bundle = new Bundle();
+        EditText userNameField = (EditText) findViewById(R.id.username);
+        EditText passwordField = (EditText) findViewById(R.id.userpassword);
+        bundle.putString("username", userNameField.getText().toString());
+        bundle.putString("password", passwordField.getText().toString());
+        startService(createCallingIntent(bundle));
+    }
+
+    private Intent createCallingIntent(Bundle bundle){
+        Intent intent = new Intent(this,LoginService.class);
+        ServerResultReceiver receiver = new ServerResultReceiver(new Handler());
+        receiver.setListener(this);
+        intent.putExtra("rec",receiver);
+        intent.putExtra("info", bundle);
+        return intent;
+    }
+
+
+    /**
+     * Prueba si el usuario ya se registro alguna vez
+     * @return true si el usuario no fue registrado, false si ya se registro alguna vez
+     */
+    private final boolean isUserRegistered(){
+        SharedPreferences data = getSharedPreferences(REGISTERED, 0);
+        return data.getBoolean(REGISTERED, false);
+        //SharedPreferences.Editor editor = data.edit();
+        //editor.
+
+    }
+
+    /**
+     * Setea un flag de que el usuario ya fue registrado
+     */
+    private final void registerUser(){
+        SharedPreferences data = getSharedPreferences(REGISTERED, 0);
+        SharedPreferences.Editor editor = data.edit();
+        editor.putBoolean(REGISTERED, true);
+    }
+
+    public void onReceiveResult(int resultCode, Bundle resultData) {
+        TextView tv = (TextView) findViewById(R.id.title);
+        String text = resultData.getString("data");
+        tv.setText(text);
+        registerUser();
+    }
+}
