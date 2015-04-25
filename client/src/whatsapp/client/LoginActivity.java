01d0b284f234357d2031a182e1693e8dbd1a74a9
Rodrigo Burdet <rodrigoburdet@gmail.com>
2015-04-25 20:29:16 -0300

    se termina #2 estructura basica del cliente parte de #11 interfaz de usuario. ahora tenemos #31 checkin de usuario, del lado de la vista, falta conectarse con servidor (proximos commits) tenemos ip configurable, se cierra #37 Tambien agregamos pantalla de registracion, #38 terminado cerrado #39 cuadros de dialogo informativos

--- client/src/whatsapp/client/LoginActivity.java
+++ client/src/whatsapp/client/LoginActivity.java
@@ -13,64 +13,79 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class LoginActivity extends Activity implements ServerResultReceiver.Listener{
-    private static final String REGISTERED = "registered";
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        if (!isUserRegistered())
-            setContentView(R.layout.login);
-        else
-        {
-            Log.v("REGISTRO", "tengo que a la proxima activity porque ya estoy registrado");
-            startActivity(new Intent(this, MainActivity.class));
-        }
-    }
+public class LoginActivity extends Activity implements ServerResultReceiver.Listener {
 
-    public void register(View v){
-        Bundle bundle = new Bundle();
-        EditText userNameField = (EditText) findViewById(R.id.username);
-        EditText passwordField = (EditText) findViewById(R.id.userpassword);
-        bundle.putString("username", userNameField.getText().toString());
-        bundle.putString("password", passwordField.getText().toString());
-        startService(createCallingIntent(bundle));
-    }
+	private static final String REGISTERED = "registered";
 
-    private Intent createCallingIntent(Bundle bundle){
-        Intent intent = new Intent(this,LoginService.class);
-        ServerResultReceiver receiver = new ServerResultReceiver(new Handler());
-        receiver.setListener(this);
-        intent.putExtra("rec",receiver);
-        intent.putExtra("info", bundle);
-        return intent;
-    }
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//TODO: Si el usuario ya se registro tendria que recordar ese usuario
+		//if (!isUserRegistered())
+		setContentView(R.layout.login);
 
+	}
 
-    /**
-     * Prueba si el usuario ya se registro alguna vez
-     * @return true si el usuario no fue registrado, false si ya se registro alguna vez
-     */
-    private final boolean isUserRegistered(){
-        SharedPreferences data = getSharedPreferences(REGISTERED, 0);
-        return data.getBoolean(REGISTERED, false);
+	public void register(View v) {
+		startActivity(new Intent(this, RegisterActivity.class));
+
+	}
+
+	public void login(View v) {
+		//TODO: verificar campos
+		Bundle bundle = new Bundle();
+		EditText userNameField = (EditText) findViewById(R.id.username);
+		EditText passwordField = (EditText) findViewById(R.id.userpassword);
+		bundle.putString("username", userNameField.getText().toString());
+		bundle.putString("password", passwordField.getText().toString());
+		bundle.putString("endpoint", "auth");
+		InfoDialog.createProgressDialog(this, "Registrando... por favor espere");
+		startService(createCallingIntent(bundle));
+	}
+
+	private Intent createCallingIntent(Bundle bundle) {
+		Intent intent = new Intent(this, LoginService.class);
+		ServerResultReceiver receiver = new ServerResultReceiver(new Handler());
+		receiver.setListener(this);
+		intent.putExtra("rec", receiver);
+		intent.putExtra("info", bundle);
+		return intent;
+	}
+
+	/**
+	 * Prueba si el usuario ya se registro alguna vez
+	 *
+	 * return true si el usuario no fue registrado, false si ya se registro
+	 * alguna vez
+	 */
+	private final boolean isUserRegistered() {
+		SharedPreferences data = getSharedPreferences(REGISTERED, 0);
+		return data.getBoolean(REGISTERED, false);
         //SharedPreferences.Editor editor = data.edit();
-        //editor.
+		//editor.
 
-    }
+	}
 
-    /**
-     * Setea un flag de que el usuario ya fue registrado
-     */
-    private final void registerUser(){
-        SharedPreferences data = getSharedPreferences(REGISTERED, 0);
-        SharedPreferences.Editor editor = data.edit();
-        editor.putBoolean(REGISTERED, true);
-    }
+	
+	/**
+	 * Setea un flag de que el usuario ya fue registrado
+	 */
+	private final void registerUser() {
+		SharedPreferences data = getSharedPreferences(REGISTERED, 0);
+		SharedPreferences.Editor editor = data.edit();
+		editor.putBoolean(REGISTERED, true);
+	}
 
-    public void onReceiveResult(int resultCode, Bundle resultData) {
-        registerUser();
-        startActivity(new Intent(this, MainActivity.class));
-    }
+	public void onReceiveResult(int resultCode, Bundle resultData) {
+		//Si fue correcto:
+		if (resultCode == 0) {
+			registerUser();
+		}
+		registerUser();
+		InfoDialog.disposeDialog();
+		startActivity(new Intent(this, MainActivity.class));
+	}
 }
