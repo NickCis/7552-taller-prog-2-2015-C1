e0a9470b3b3624ee06fb3ac1f9f6851e2303ce9e
Nicolas Cisco <n.cis_92@hotmail.com>
2015-04-26 17:34:22 -0300

    #26 RootNode incluye NotificationNode

--- server/src/rest/root_node.cpp
+++ server/src/rest/root_node.cpp
@@ -2,11 +2,13 @@
 #include "signup_node.h"
 #include "user_node.h"
 #include "auth_node.h"
+#include "notification_node.h"
 #include "not_found_node.h"
 
 RootNode::RootNode() : WAParentNode("/") {
 	children.push_back(new SignupNode());
 	children.push_back(new UserNode());
 	children.push_back(new AuthNode());
+	children.push_back(new NotificationNode());
 	children.push_back(new NotFoundNode());
 }
