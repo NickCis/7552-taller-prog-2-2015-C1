af68c2ae3582a1fd7b2b5ab8b4ce6db0768e32fd
Nicolas Cisco <n.cis_92@hotmail.com>
2015-04-26 17:41:41 -0300

    #26 Andan notificaciones a mensajes. Falta marcar notificaciones como leidas

--- server/test.sh
+++ server/test.sh
@@ -1,17 +1,23 @@
 #! /bin/bash
-curl --data "user=pepe&pass=123456789" http://localhost:8000/signup && echo && sleep 0.1
-curl --data "user=pepepe&pass=123456789" http://localhost:8000/signup && echo && sleep 0.1
-curl --data "user=foo&pass=123456789" http://localhost:8000/signup && echo && sleep 0.1
-curl --data "user=foo&pass=123456789" http://localhost:8000/auth && echo && sleep 0.1
+echo "Creo usuario Pepe"
+curl -s --data "user=pepe&pass=123456789" http://localhost:8000/signup && echo
+echo "Creo usuario Foo"
+curl -s --data "user=foo&pass=123456789" http://localhost:8000/signup && echo
 
-TOKEN=$(curl --data "user=foo&pass=123456789" http://localhost:8000/auth 2>&1 |  sed  -n 's/^[^:]*: "\([^"]*\)".*$/\1/p')
+echo "Conecto a Pepe"
+TOKEN_PEPE=$(curl -s --data "user=pepe&pass=123456789" http://localhost:8000/auth 2>&1 |  sed  -n 's/^[^:]*: "\([^"]*\)".*$/\1/p')
+echo "Token de Pepe: $TOKEN_PEPE"
+echo "Conecto a Foo"
+TOKEN_FOO=$(curl -s --data "user=foo&pass=123456789" http://localhost:8000/auth 2>&1 |  sed  -n 's/^[^:]*: "\([^"]*\)".*$/\1/p')
+echo "Token de Foo: $TOKEN_FOO"
 
-echo "token a usar $TOKEN"
+echo "Foo manda mensajes a pepe"
+curl -s --data "access_token=$TOKEN_FOO&message=primer mensaje a pepe" http://localhost:8000/user/pepe/messages && echo
+curl -s --data "access_token=$TOKEN_FOO&message=segundo mensaje a pepe" http://localhost:8000/user/pepe/messages && echo
+curl -s --data "access_token=$TOKEN_FOO&message=tercer mensaje a pepe" http://localhost:8000/user/pepe/messages && echo
+curl -s --data "access_token=$TOKEN_FOO&message=cuarto mensaje a pepe" http://localhost:8000/user/pepe/messages && echo
+curl -s --data "access_token=$TOKEN_FOO&message=quinto mensaje a pepepe" http://localhost:8000/user/pepe/messages && echo
+curl -s --data "access_token=$TOKEN_FOO&message=sexto mensaje a pepe" http://localhost:8000/user/pepe/messages && echo
 
-curl --data "access_token=$TOKEN&message=primer mensaje a pepe" http://localhost:8000/user/pepe/messages && echo && sleep 0.1
-curl --data "access_token=$TOKEN&message=primer mensaje a pepepe" http://localhost:8000/user/pepepe/messages && echo && sleep 0.1
-curl --data "access_token=$TOKEN&message=segundo mensaje a pepe" http://localhost:8000/user/pepe/messages && echo && sleep 0.1
-curl --data "access_token=$TOKEN&message=tercer mensaje a pepe" http://localhost:8000/user/pepe/messages && echo && sleep 0.1
-curl --data "access_token=$TOKEN&message=segundo mensaje a pepepe" http://localhost:8000/user/pepepe/messages && echo && sleep 0.1
-curl --data "access_token=$TOKEN&message=cuarto mensaje a pepe" http://localhost:8000/user/pepe/messages && echo
-
+echo "Notificaciones de pepe"
+curl -s "http://localhost:8000/notification?access_token=$TOKEN_PEPE" && echo
