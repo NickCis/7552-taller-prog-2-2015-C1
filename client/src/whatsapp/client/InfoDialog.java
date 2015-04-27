a2c7c4d8c254dc827d67dabc0357d9fe580df1c3
Rodrigo Burdet <rodrigoburdet@gmail.com>
2015-04-27 04:26:46 -0300

    Primeras 2 pantallas completas, Registracion y login funcionando bien, con ip configurable y con memoria, cuadros de dialogo mas informativos. La clase CustomRequest sirve para terminar con el problema que tenia de que el JSONRequest mandaba parametros nulos.

--- client/src/whatsapp/client/InfoDialog.java
+++ client/src/whatsapp/client/InfoDialog.java
@@ -5,8 +5,11 @@
  */
 package whatsapp.client;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 
 /**
  *
@@ -14,6 +17,7 @@ import android.content.Context;
  */
 public class InfoDialog {
 	private static ProgressDialog dialog;
+	private static AlertDialog alertDialog;
 	
 	public static ProgressDialog createProgressDialog(Context ctx, String msg){
 		dialog = new ProgressDialog(ctx);
@@ -23,10 +27,27 @@ public class InfoDialog {
         dialog.show();
 		return dialog;
 	}
+	public static AlertDialog createAlertDialog(final Context ctx, String title, String msg,final Class<?> cls){
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ctx);
+		alertDialogBuilder.setMessage(msg);
+		alertDialogBuilder.setTitle(title);
+		alertDialogBuilder.setNeutralButton("Aceptar", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface di, int i) {
+				di.dismiss();
+				ctx.startActivity(new Intent(ctx, cls));
+
+			}
+		});
+		alertDialog = alertDialogBuilder.create();
+		alertDialog.show();
+		return alertDialog;
+	}
 	
 	public static void disposeDialog(){
-		dialog.dismiss();
+		if (dialog!=null && dialog.isShowing())
+			dialog.dismiss();
 	}
+	
 	
 	
 	
