1e894bfcf77f8013854139b6b3883e264f0d1fbd
Nicolas Cisco <n.cis_92@hotmail.com>
2015-04-27 01:30:12 -0300

    #42 se sube documentacion doxygen de todas las clases base de nodos

--- server/src/node/wa_method_node.h
+++ server/src/node/wa_method_node.h
@@ -6,14 +6,31 @@
 
 class WAMethodNode : public WAStrNode {
 	public:
+		/** Constructor
+		 * @param str: ppcio de la url que manejara
+		 */
 		WAMethodNode(const std::string &str);
 		WAMethodNode(const char *str);
 
 	protected:
+		/** Chequea el metodo (METHOD HTTP) de la connecion y ejecuta el metodo correspondiente
+		 */
 		void execute(MgConnection& conn, const char* url);
+
+		/** Metodo que se ejecuta si la connecion es POST
+		 */
 		virtual void executePost(MgConnection& conn, const char* url);
+
+		/** Metodo que se ejecuta si la connecion es GET
+		 */
 		virtual void executeGet(MgConnection& conn, const char* url);
+
+		/** Metodo que se ejecuta si la connecion es DELETE
+		 */
 		virtual void executeDelete(MgConnection& conn, const char* url);
+
+		/** Manejador de metodo no implementado
+		 */
 		virtual void methodNotAllowed(MgConnection& conn, const char* url);
 };
 
--- server/src/node/wa_node.h
+++ server/src/node/wa_node.h
@@ -3,13 +3,29 @@
 
 #include "../mg/mg_connection.h"
 
+/** Nodo de la web
+ */
 class WANode {
 	public:
 		virtual ~WANode();
+		/** Manejador del nodo.
+		 * @param conn: coneccion
+		 * @param url: url actual (o parte, los nodos adelantaran el ppcio antes de pasarselas a subnodos)
+		 * @return true: si fue manejado, false si no
+		 */
 		bool handle(MgConnection& conn, const char* url);
 
 	protected:
+		/** metodo que dice si este nodo manejara la url proporcionada
+		 * @param url
+		 * @return true: si la debe manejar, false: si no
+		 */
 		virtual bool match(const char* url) = 0;
+
+		/** Metodo que raliza las acciones que el nodo deba hacer
+		 * @param conn coneccion
+		 * @param url: url actual (o parte, los nodos adelantaran el ppcio antes de pasarselas a subnodos)
+		 */
 		virtual void execute(MgConnection& conn, const char* url) = 0;
 };
 
