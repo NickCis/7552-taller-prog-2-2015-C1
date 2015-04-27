c9b0b5df45528fc9af29599965fd45f8d9830485
Nicolas Cisco <n.cis_92@hotmail.com>
2015-04-27 01:20:12 -0300

    #42 se sube documentacion doxygen de todas las clases que abstraen a mongoose

--- server/src/mg/mg_server.h
+++ server/src/mg/mg_server.h
@@ -16,6 +16,9 @@ extern "C" {
  */
 class MgServer {
 	public:
+		/** Constructor
+		 * @param threads: cantidad de threads a utilizar
+		 */
 		MgServer(int threads = 1);
 		~MgServer();
 
@@ -24,19 +27,34 @@ class MgServer {
 		 */
 		const char* setPort(int port=80);
 
+		/** Setea opciones, abstraccion de mg_set_option
+		 */
 		const char* setOption(const std::string& name, const std::string& value);
 		const char* setOption(const char*name, const char*value);
 
+		/** Arranca servidor
+		 */
 		void run();
+
+		/** Para servidor
+		 */
 		void stop();
 
 	protected:
+		/** Handler que usa el mg_create_server para despachar las conecciones
+		 */
 		static int handlerCaller(struct mg_connection*, enum mg_event);
+
+		/** Funcion de thread
+		 */
 		static void* threadHandler(void*);
+
+		/** Hanlder que maneja la coneccion
+		 */
 		virtual int handler(MgConnection& conn, enum mg_event ev) = 0;
 
-		std::vector<struct mg_server*> servers;
-		std::vector<pthread_t> threads;
+		std::vector<struct mg_server*> servers; ///< Vector de servers utilizados (uno por cada thread)
+		std::vector<pthread_t> threads; ///< vector de threads corriendo
 
 		int running;
 };
