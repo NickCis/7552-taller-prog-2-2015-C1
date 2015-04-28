e3d9d0750de7dd521f8361c324a587ecfcd77a8d
NickCis <ncis20@gmail.com>
2015-04-28 17:46:43 -0300

    #46 Se agrega test de WA_Node

---
+++ server/test/node/wa_node.cpp
@@ -0,0 +1,45 @@
+#include "../catch.hpp"
+#include "../../src/node/wa_node.h"
+
+/** Mock de MgConnection **/
+MgConnection::MgConnection(struct mg_connection*){
+}
+
+class MockWaNode : public WANode {
+	public:
+		MockWaNode() {}
+		bool deboMatchear;
+		bool ejecuteExecute;
+
+	protected:
+		virtual bool match(const char*){
+			return deboMatchear;
+		}
+		virtual void execute(MgConnection&, const char*){
+			ejecuteExecute = true;
+		}
+};
+
+TEST_CASE( "Comprobar el funcionamiento de NotFoundNode", "[NotFoundNode]" ) {
+	MockWaNode waNode;
+	MgConnection conn(NULL);
+
+	SECTION( "Si matchea debe ejecutar execute" ) {
+		waNode.deboMatchear = true;
+		waNode.ejecuteExecute = false;
+		bool ret = waNode.handle(conn, "");
+
+		REQUIRE( ret == true );
+		REQUIRE( waNode.ejecuteExecute == true );
+	}
+
+	SECTION( "Si no matchea debe no ejecutar execute" ) {
+		waNode.deboMatchear = false;
+		waNode.ejecuteExecute = false;
+		bool ret = waNode.handle(conn, "");
+
+		REQUIRE( ret == false );
+		REQUIRE( waNode.ejecuteExecute == false );
+	}
+}
+
--- server/test/rest/not_found_node.cpp
+++ server/test/rest/not_found_node.cpp
@@ -1,4 +1,3 @@
-#define CATCH_CONFIG_MAIN
 #include "../catch.hpp"
 #include "../../src/rest/not_found_node.h"
 
