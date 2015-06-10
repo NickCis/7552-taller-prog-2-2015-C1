c2d4c42e155ddb968d2f01c9db69c61024f13a17
MatSebMan <matiasmanzano@yahoo.com.ar>
2015-06-10 18:09:20 -0300

    Agregada interfaz de galeria de imagenes para la seleccion de imagen de perfil. resta actualizar Base de Datos, agregar datos restantes y corregir un bug menor que no toma la seleccion de la primer imagen mostrada. Ticket #59

--- client/src/whatsapp/client/ProfileConfigurationActivity.java
+++ client/src/whatsapp/client/ProfileConfigurationActivity.java
@@ -5,7 +5,19 @@
 package whatsapp.client;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,6 +30,58 @@ public class ProfileConfigurationActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_configuration);
+        ImageView imageView = (ImageView) findViewById(R.id.userProfileAvatar);
+        
     }
     
+    public void addImage(View v)
+    {
+        startActivityForResult(new Intent(this, GalleryActivity.class), 1);
+    }
+    
+    @Override 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) 
+    {
+        super.onActivityResult(requestCode, resultCode, data); 
+        switch(requestCode) 
+        {
+            case (1) : 
+            {
+                if (resultCode == Activity.RESULT_OK) 
+                {
+                    ImageView imageView = (ImageView) findViewById(R.id.userProfileAvatar);
+                    imageView.setImageBitmap(decodeFile(data.getStringExtra("filepath"), 200,200));
+                }
+                break; 
+            } 
+        } 
+    }
+    
+    /*
+     * Resizing image size
+     */
+    public static Bitmap decodeFile(String filePath, int WIDTH, int HIGHT) {
+        try {
+ 
+            File f = new File(filePath);
+ 
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(new FileInputStream(f), null, o);
+ 
+            final int REQUIRED_WIDTH = WIDTH;
+            final int REQUIRED_HIGHT = HIGHT;
+            int scale = 1;
+            while (o.outWidth / scale / 2 >= REQUIRED_WIDTH
+                    && o.outHeight / scale / 2 >= REQUIRED_HIGHT)
+                scale *= 2;
+ 
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
